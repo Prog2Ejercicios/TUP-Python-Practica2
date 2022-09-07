cf6702b5e7a17688159c9f04b3e3dbd297c7da82
Clara <91093218+ClaraRazzetto@users.noreply.github.com>
2022-09-07 00:51:37 -0300

    practica 2 ejerciciosX

--- exercises/tempCodeRunnerFile.py
+++ exercises/tempCodeRunnerFile.py
@@ -1,29 +1,5 @@
-
-# numeros_al_cubo = map(lambda x: x ** 3, numeros) # Completar
-
-
-# """
-# Escribir una función lambda que permita filtrar todos los elementos pares
-
-# Restricción: Utilizar List, filter, lambda y la variable numeros_al_cubo
-# """
-
-# numeros_al_cubo_pares = filter(lambda x: not (x%2) != 0, numeros_al_cubo) # Completar
-
-
-# """
-# Escribir una función Lambda que sume todos los elementos
-
-# Restricción: Utilizar reduce, lambda y la variable numeros_al_cubo_pares
-# """
-
-# from functools import reduce  # noqa: E402
-
-# suma_numeros_al_cubo_pares = reduce(lambda x,y: x + y, numeros_al_cubo_pares) # Completar
-
-
-# # NO MODIFICAR - INICIO
-# assert numeros_al_cubo == [1, 8, 27, 64, 125, 216]
-# assert numeros_al_cubo_pares == [8, 64, 216]
-# assert suma_numeros_al_cubo_pares == 288
-# # NO MODIFICAR - FIN
+if __name__ == "__main__":
+#     assert maximo_recursivo(1, 10, 5, -5) == 10
+#     assert maximo_recursivo(4, 9, 18, 6) == 18
+#     assert maximo_recursivo(24, 9, 18, 20) == 24
+#     assert maximo_recursivo(24, 9, 18, 30) == 30
