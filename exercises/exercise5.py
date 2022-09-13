64b9f2b6ea25d66c12fd7eedd9746b41985b34c2
lucas <lc45648siete@gmail.com>
2022-09-12 21:24:25 -0300

    Terminado

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -1,12 +1,19 @@
 """For, Sum, Reduce."""
 
 
+from re import I
+
+
 def sumatoria_basico(n: int) -> int:
     """Devuelve la suma de los números de 1 a N.
 
     Restricción: Utilizar un bucle FOR.
     """
-
+  
+    suma = 0
+    for num in range(n+1):
+        suma = suma + num     
+    return suma 
 
 # NO MODIFICAR - INICIO
 assert sumatoria_basico(1) == 1
@@ -23,8 +30,8 @@ def sumatoria_sum(n: int) -> int:
     Restricción: No utilizar bucles (FOR, WHILE, etc)
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
-
-
+    return sum(range(n), n)
+    
 # NO MODIFICAR - INICIO
 assert sumatoria_sum(1) == 1
 assert sumatoria_sum(100) == 5050
@@ -38,7 +45,7 @@ from typing import Iterable  # noqa: E402
 
 
 def multiplicar_basico(numeros: Iterable[float]) -> float:
-    """Toma un lista de números y devuelve el producto todos los númereos. Si
+    """Toma un lista de números y devuelve el multiplicacion todos los númereos. Si
     la lista está vacia debe devolver 0.
 
     Restricciones:
@@ -47,8 +54,17 @@ def multiplicar_basico(numeros: Iterable[float]) -> float:
         - Utilizar múltiples Return
         - No utilizar ELSE
     """
+    
+    multiplicacion = 1
+    if numeros ==[]:
+        return 0
+
+    for i in numeros:
+        multiplicacion*=i
+    return multiplicacion 
 
 
+    
 # NO MODIFICAR - INICIO
 assert multiplicar_basico([1, 2, 3, 4]) == 24
 assert multiplicar_basico([2, 5]) == 10
