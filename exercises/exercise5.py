301876d3dae8a52d292679b1c1bd97a004565375
ROXANA291088 <63433371+ROXANA291088@users.noreply.github.com>
2022-09-14 21:58:14 -0300

    Roxana Pissani 50607

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -6,6 +6,10 @@ def sumatoria_basico(n: int) -> int:
 
     Restricción: Utilizar un bucle FOR.
     """
+    suma = 0
+    for i in range(n+1):
+        suma += i
+    return suma
 
 
 # NO MODIFICAR - INICIO
@@ -23,7 +27,7 @@ def sumatoria_sum(n: int) -> int:
     Restricción: No utilizar bucles (FOR, WHILE, etc)
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
-
+    return sum(range(1 ,n+1))
 
 # NO MODIFICAR - INICIO
 assert sumatoria_sum(1) == 1
@@ -47,6 +51,14 @@ def multiplicar_basico(numeros: Iterable[float]) -> float:
         - Utilizar múltiples Return
         - No utilizar ELSE
     """
+    producto = 1
+
+    if numeros ==[]:
+        return 0
+
+    for i in numeros:
+        producto*= i
+    return producto
 
 
 # NO MODIFICAR - INICIO
--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -11,7 +11,11 @@ def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool
         - Utilizar dos bucles FOR anidados.
         - Utilizar dos returns.
     """
-
+    for i in lista_1:
+        for j in lista_2:
+            if i == j:
+                return True
+    return False
 
 # NO MODIFICAR - INICIO
 test_list = [1, "hello", 35.20]
@@ -30,6 +34,10 @@ def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
         - Utilizar un único bucle FOR.
         - Utilizar dos returns.
     """
+    for i in lista_1:
+        if i in lista_2:
+            return True
+    return False
 
 
 # NO MODIFICAR - INICIO
@@ -52,6 +60,7 @@ def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
 
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
+    return any([i for i in lista_1 if i in lista_2])
 
 
 # NO MODIFICAR - INICIO
@@ -73,6 +82,9 @@ def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
 
     Referencia: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset  # noqa: E501
     """
+    conjunto_1 = set(lista_1)
+    conjunto_2 =  set(lista_2)
+    return not conjunto_1.isdisjoint(conjunto_2)
 
 
 # NO MODIFICAR - INICIO
