3637abe6ce82ffa6e8d61065269cee8be16d48bd
Francisco Skvosnakov <franciscoskvuso@gmail.com>
2022-09-14 21:49:52 -0300

    ¨practica2¨

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -1,69 +1,88 @@
-"""Expresiones Booleanas."""
+"""Any y Sets."""
+
+from shutil import register_unpack_format
+from typing import Any, Iterable
 
 
-def es_vocal_if(letra: str) -> bool:
-    """Toma un string y devuelve un booleano en base a si letra es una vocal o
-    no.
-
-    Restricciónes:
-        - Utilizar un if para cada posibilidad.
-        - Utilizar la función lower() sólo una vez.
-        - No utilizar ELSE.
-        - Utilizar 6 returns.
-
-    Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
-    """
-
-
-# NO MODIFICAR - INICIO
-assert es_vocal_if("a")
-assert not es_vocal_if("b")
-assert es_vocal_if("A")
-assert es_vocal_if("e")
-assert es_vocal_if("E")
-# NO MODIFICAR - FIN
-
-
-###############################################################################
-
-
-def es_vocal_if_in(letra: str) -> bool:
-    """Re-escribir utilizando un sólo IF y el operador IN.
-
-    Restricciónes:
-        - Utilizar un único IF.
+def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:  # noqa: E501
+    for i in lista_1:
+        for j in lista_2:
+            if i == j:
+                return True
+    return False
+    """Toma dos listas y devuelve un booleano en base a si tienen al menos 1
+    elemento en común.
+    Restricciones:
+        - Utilizar dos bucles FOR anidados.
         - Utilizar dos returns.
-        - No utilizar ELSE.
-        - No utilizar FOR.
-        - No utilizar listas.
-
-    Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations # noqa: E501
     """
 
 
 # NO MODIFICAR - INICIO
-assert es_vocal_if_in("a")
-assert not es_vocal_if_in("b")
-assert es_vocal_if_in("A")
+test_list = [1, "hello", 35.20]
+assert superposicion_basico(test_list, (2, "world", 35.20))
+assert not superposicion_basico(test_list, (2, "world", 30.85))
+# NO MODIFICAR - FIN
+
+
+# ###############################################################################
+
+
+def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
+    for i in lista_1:
+        if i in lista_2:
+            return True
+    return False
+    """Re-Escribir utilizando un sólo bucle y el operador IN.
+    Restricciones:
+        - Utilizar un único bucle FOR.
+        - Utilizar dos returns.
+    """
+
+
+# NO MODIFICAR - INICIO
+test_list = [1, "hello", 35.20]
+assert superposicion_in(test_list, (2, "world", 35.20))
+assert not superposicion_in(test_list, (2, "world", 30.85))
+# NO MODIFICAR - FIN
+
+
+# ###############################################################################
+
+
+def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
+    return any([x for x in lista_1 if x in lista_2])
+    """Re-Escribir utilizando la funcion any.
+    Restricciones:
+        - No utilizar bucles.
+        - Utilizar una comprensión.
+        - La solución debe tener 1 línea.
+    Referencia: https://docs.python.org/3/library/functions.html#any
+    """
+
+
+# NO MODIFICAR - INICIO
+test_list = [1, "hello", 35.20]
+assert superposicion_any(test_list, (2, "world", 35.20))
+assert not superposicion_any(test_list, (2, "world", 30.85))
 # NO MODIFICAR - FIN
 
 
 ###############################################################################
 
 
-def es_vocal_in(letra: str) -> bool:
-    """Re-escribir como expresión booleana utilizando el operador IN
-
-    Restricciónes:
-        - No utilizar IF.
-        - Utilizar un único return.
-        - No utilizar FOR.
-        - No utilizar listas.
+def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
+    return not set(lista_1).isdisjoint(set(lista_2))
+    """Re-Escribir utilizando conjuntos (sets).
+    Restricciones:
+        - Resolver sólo utilizando operaciones de conjuntos
+        - No utilizar ANY, ALL, FOR, IF ni COMPRENSIONES
+    Referencia: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset  # noqa: E501
     """
 
 
 # NO MODIFICAR - INICIO
-assert es_vocal_in("a")
-assert not es_vocal_in("b")
-assert es_vocal_in("A")
+test_list = [1, "hello", 35.20]
+assert superposicion_set(test_list, (2, "world", 35.20))
+assert not superposicion_set(test_list, (2, "world", 30.85))
 # NO MODIFICAR - FIN
